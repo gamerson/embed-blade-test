09b2a2daec0b5a31366afe2099ed12256b0662f7
Gregory Amerson <gregory.amerson@liferay.com>
2018-09-18 10:29:19 -0500

    try/catch

--- impl/src/main/java/com/liferay/tools/test/embedblade/BladeInit.java
+++ impl/src/main/java/com/liferay/tools/test/embedblade/BladeInit.java
@@ -4,6 +4,7 @@ import com.liferay.blade.cli.BladeCLI;
 import com.liferay.blade.cli.command.InitArgs;
 import com.liferay.blade.cli.command.InitCommand;
 
+import java.io.IOException;
 import java.nio.file.Files;
 
 import org.osgi.service.component.annotations.Component;
@@ -17,16 +18,24 @@ import org.osgi.service.component.annotations.Component;
 )
 public class BladeInit {
 
-  public void init() throws Exception {
-	  BladeCLI bladeCLI = new BladeCLI();
+  public void init() {
+	  InitArgs initArgs;
+	try {
+		BladeCLI bladeCLI = new BladeCLI();
+		  
+		  InitCommand initCommand = new InitCommand();
+		  initArgs = new InitArgs();
+		  initArgs.setBase(Files.createTempDirectory("init").toFile());
+		  initCommand.setArgs(initArgs);
+		  initCommand.setBlade(bladeCLI);
+		  
+		  initCommand.execute();
+		  System.out.println("Created new workspace in " + initArgs.getBase());
+	}
+	catch (Exception e) {
+		e.printStackTrace();
+	}
 	  
-	  InitCommand initCommand = new InitCommand();
-	  InitArgs initArgs = new InitArgs();
-	  initArgs.setBase(Files.createTempDirectory("init").toFile());
-	  initCommand.setBlade(bladeCLI);
-	  
-	  initCommand.execute();
-	  System.out.println("Created new workspace in " + initArgs.getBase());
   }
 
 }
